e107e7ebb0e2c11dbe86f122383777487d4c1163
kekosh <kenta0540@gmail.com>
2020-01-13 23:44:14 +0900

    first commit

---
+++ src/components/Addresses.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>マイアドレス帳</h1>
+</div>
+</template>
+
+<!-- javascriptを書くためのエリア -->
+<script>
+</script>
+
+<!-- scoped lang はscssでcssを書くための属性 -->
+<style scoped lang="scss">
+</style>
